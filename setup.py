e45d417a1a240f9505a9cd544775d6940d939bd0
Giles Edkins <edkins@gmail.com>
2017-09-06 16:53:18 -0400

    Get credentials from secret file

--- setup.py
+++ setup.py
@@ -1,27 +1,73 @@
+import requests
+import time
+import os
+import json
 
 name = 'optimalmemes.org'
 
-def resolve_name():
-    raise Error('not implemented')
+def secret():
+    if not os.path.exists('secret'):
+        raise Exception('Need secret file with digitalocean_token, digitalocean_ssh_name, godaddy_key, godaddy_secret')
+    return json.loads(open('secret').read())
 
-def start_droplet():
-    response = start_droplet_through_api()
-    add_known_host(response.ip, response.host_key)
-    return response.ip
+def bearer():
+    return 'Bearer ' + secret()['digitalocean_token']
 
-def droplet_ip():
-    ip = find_droplet()
-    if ip == None:
-        return ip
-    return start_droplet()
+def sso_key():
+    return 'sso-key ' + secret()['godaddy_key'] + ':' + secret()['godaddy_secret']
 
-def server_ip():
-    if not have_known_host():
-        ip = droplet_ip()
-        if resolve_name() != ip:
-            godaddy_set_name_to(ip)
+def droplets():
+    r = requests.get('https://api.digitalocean.com/v2/droplets', headers={'Authorization':bearer()})
+    r.raise_for_status()
+    return r.json()['droplets']
 
-def ssh_in_and_do_things():
-    server_ip()
-    ssh()
+def find_key_id():
+    r = requests.get('https://api.digitalocean.com/v2/account/keys', headers={'Authorization':bearer()})
+    r.raise_for_status()
+    key_name = secret()['digitalocean_ssh_name']
+    for ssh_key in r.json()['ssh_keys']:
+        if ssh_key['name'] == key_name:
+            return ssh_key['id']
+    raise Exception('Cannot find ssh key named %s' %(key_name,))
+
+def create_droplet():
+    key_id = find_key_id()
+    payload = {'name':name, 'region':'nyc1','size':'512mb','image':'ubuntu-16-04-x64','ssh_keys':[key_id],'backups':False,'ipv6':False,'user_data':None,'private_networking':None,'volumes':None,'tags':[]}
+    r = requests.post('https://api.digitalocean.com/v2/droplets', data=json.dumps(payload), headers={'Content-Type':'application/json','Authorization':bearer()})
+    if r.status_code >= 400:
+        print(r.text)
+    r.raise_for_status()
+
+def have_droplet():
+    for droplet in droplets():
+        if droplet['name'] == name:
+            return True
+    return False
+
+def get_droplet_ip():
+    for droplet in droplets():
+        if droplet['name'] == name and 'networks' in droplet and 'v4' in droplet['networks'] and len(droplet['networks']['v4']) > 0:
+            return droplet['networks']['v4'][0]['ip_address']
+    return None
+
+def get_droplet_ip_or_create():
+    if not have_droplet():
+        print('Creating droplet')
+        create_droplet()
+        print('Droplet successfully created')
+    for i in range(10):
+        ip = get_droplet_ip()
+        if ip != None:
+            return ip
+        time.sleep(5)
+
+    raise Exception('Droplet did not return ip address')
+
+def domains():
+    r = requests.get('https://api.godaddy.com/v1/domains', headers={'Authorization':sso_key()})
+    print(r.json())
+    r.raise_for_status()
+
+#print(get_droplet_ip_or_create())
+print(domains())
 
